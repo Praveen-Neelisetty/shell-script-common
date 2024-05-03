d3cf51eb77fe785690489de6ebd3f673eb8bfe26
Praveen <saipraveenneelisetty27@gmail.com>
2024-05-03 22:23:42 +0530

    Added Common file for common code

--- mySql.sh
+++ mySql.sh
@@ -1,37 +1,8 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | awk -F "." '{print $1F}')
-#SCRIPT_NAME=$(echo $0 | awk -F "." '{print $1F}')
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 read -r -p "Enter You MySQL Password: " MySqlPassword
-# echo "Enter You MySQL Password: ${MySqlPassword}"
-# echo "Enter You MySQL Second Time for Verification Password: $MySqlPassword"
 
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "You are not Super User, Hence $R Execution STOPPED $N"
-    exit 1
-else
-    echo "You are super user"
-fi
-
-VALIDATE()
-{
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ...$R FAILED $N"
-        exit 1
-    else
-        echo -e "$2 ...$G SUCCESS $N"
-    fi
-}
+source ./common.sh
 
 #dnf list installed mysql &>>$LOGFILE "MySQL Exists or not"
 #VALIDATE $? "MySQL Exists?"
